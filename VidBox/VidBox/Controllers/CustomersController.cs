824c37e62b0f814c2b245c10098136dbcb9eebb8
James W. Tonkin <btonkin71@comcast.net>
2017-08-23 12:24:56 -0500

    Added customers and movies index view.

---
+++ VidBox/VidBox/Controllers/CustomersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidBox.Models;
+
+namespace VidBox.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public ViewResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleorDefault(c => c.Id == id);
+
+            if (customer == null)
+                return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer() {Id = 1, Name = "Jordan Nelson"},
+                new Customer() {Id = 2, Name = "Dean Smith"}
+            };
+        }
+    }
+}
--- VidBox/VidBox/Controllers/MoviesController.cs
+++ VidBox/VidBox/Controllers/MoviesController.cs
@@ -10,6 +10,22 @@ namespace VidBox.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie() {Id = 1, Name = "Django"},
+                new Movie() {Id = 2, Name = "Pulp Fiction"}
+            };
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -27,29 +43,6 @@ namespace VidBox.Controllers
             };
 
             return View(viewModel);
-        }
-
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-
-        // Movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
-
-        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
         }
     }
 }
