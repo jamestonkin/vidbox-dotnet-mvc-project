b1341ddbd5e3569aebe8bb7904ba578cda49506b
James W. Tonkin <btonkin71@comcast.net>
2017-08-23 11:08:11 -0500

    Added randommovieviewmodel and customer class.

--- VidBox/VidBox/Controllers/MoviesController.cs
+++ VidBox/VidBox/Controllers/MoviesController.cs
@@ -4,16 +4,52 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using VidBox.Models;
+using VidBox.ViewModels;
 
 namespace VidBox.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 1"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // Movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
